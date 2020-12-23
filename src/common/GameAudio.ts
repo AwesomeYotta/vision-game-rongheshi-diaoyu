a18d246974c36c354fc6b08766546bec63befc41
TranformGit <2630243397@qq.com>
2020-12-23 19:42:34 +0800

    update GameAudio

--- src/common/GameAudio.ts
+++ src/common/GameAudio.ts
@@ -2,7 +2,7 @@ import { Singleton } from "@/types";
 import { EventCenter } from "@/util/EventCenter";
 import { GameEvent } from "./GameEvent";
 import { res, ResId } from './GameRes'
-
+import messager, { ErrorType, ErrorObj } from '@/util/messager';
 export default class GameAudio extends Singleton {
     public static get i(): GameAudio {
         return super.getInstance();
@@ -46,13 +46,28 @@ export default class GameAudio extends Singleton {
     }
     /**开启或关闭声音，返回toggle后声音是否是开启状态 */
     public toggleSound() {
-        this._soundOpen = !this.soundOpen;
-        if (this.soundOpen) {
+        let errObj: ErrorObj = {
+            type: ErrorType.GAME_WARNING,
+            code: 'SHOW_ALERT',
+            message: '音效打开失败，请点击[操作说明]窗口的音效开关重试',
+            content: ''
+        };
+        if (!this.soundOpen) {
             let instance = createjs.Sound.play(ResId.bgMp3, { interrupt: createjs.Sound.INTERRUPT_ANY, loop: -1 });
             (<any>window).au = instance;
-
+            if (instance.playState !== 'playSucceeded') {
+                messager.sendError(errObj);
+            } else {
+                this._soundOpen = true;
+            }
         } else {
             createjs.Sound.stop();
+            if((<any>window).au.playState !== 'playFinished') {
+                errObj.message = errObj.message.replace('打开', '关闭');
+                messager.sendError(errObj);
+            } else {
+                this._soundOpen = false;
+            }
         }
         return this._soundOpen;
     }
